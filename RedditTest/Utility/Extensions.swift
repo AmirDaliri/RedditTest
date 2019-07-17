a9f27c21410a9f697299339f0814923a5ba21951
idaliri <idaliri69@gmail.com>
2019-07-17 20:37:25 +0300

    code conventions

---
+++ RedditTest/Utility/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  RedditTest
+//
+//  Created by Amir Daliri on 17.07.2019.
+//  Copyright © 2019 AmirDaliri. All rights reserved.
+//
+
+import Foundation
---
+++ RedditTest/Utility/Helpers.swift
@@ -0,0 +1,9 @@
+//
+//  Helpers.swift
+//  RedditTest
+//
+//  Created by Amir Daliri on 17.07.2019.
+//  Copyright © 2019 AmirDaliri. All rights reserved.
+//
+
+import Foundation
